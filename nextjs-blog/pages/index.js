b6749cebb0f1d3b3398d0c2e49f3b6edfbfcb443
Desiree Mendes <desireefrost@live.ca>
2022-02-16 13:05:31 -0500

    Polish index page

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import { Link } from 'next/link'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -20,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello there, I'm learning Next.js!</p>
+        <p>Hi, I'm Desiree. I'm a software developer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
